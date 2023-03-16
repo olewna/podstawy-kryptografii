af7cb894e69616c9731c5145bb5fef7a9dc35b72
olewna <dajcie.suba2002@gmail.com>
2023-03-16 14:53:37 +0100

    affine cryptanalisis

--- lab01/zad01.js
+++ lab01/zad01.js
@@ -6,8 +6,8 @@ const tekst = [];
 const klucz = [];
 const extra = [];
 
-const szyfr = "c"; //c a
-const odszyfrowywanie = "k"; //e d j k
+const szyfr = "a"; //c a
+const odszyfrowywanie = "j"; //e d j k
 
 if (szyfr === "c" && odszyfrowywanie === "d") {
   try {
@@ -138,6 +138,41 @@ if (szyfr === "c" && odszyfrowywanie === "d") {
       console.error(err);
     }
   }
+} else if (szyfr === "a" && odszyfrowywanie === "j") {
+  try {
+    const data = fs.readFileSync("crypto.txt", "utf8");
+    tekst.push(...data.split(""));
+    const data2 = fs.readFileSync("extra.txt", "utf8");
+    extra.push(...data2.split(""));
+  } catch (err) {
+    console.error(err);
+  }
+
+  const wynik = afinicznyKryptoanalizaZTekstem(tekst, extra);
+
+  try {
+    fs.writeFileSync("decrypt.txt", wynik[1]);
+    fs.writeFileSync("key-found.txt", wynik[0].join(" "));
+    console.log("Zapisano do pliku");
+  } catch (err) {
+    console.error(err);
+  }
+} else if (szyfr === "a" && odszyfrowywanie === "k") {
+  try {
+    const data = fs.readFileSync("crypto.txt", "utf8");
+    tekst.push(...data.split(""));
+  } catch (err) {
+    console.error(err);
+  }
+
+  const mozliwyTekst = afinicznyKryptoanaliza(tekst);
+
+  try {
+    fs.writeFileSync("decrypt.txt", mozliwyTekst);
+    console.log("Zapisano do pliku");
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 function cezarSzyfrowanie(tekst, przesuniecie) {
@@ -166,6 +201,25 @@ function cezarOdszyfrowanie(tekst, przesuniecie) {
     .map((x) => (typeof x === "number" ? String.fromCharCode(x) : x))
     .join("");
   return zaszyfrowane;
+}
+
+function cezarKryptoanalizaZTekstemJawnym(tekst, extra) {
+  let szyfrLiczba = tekst[0].charCodeAt(0);
+  let extraLiczba = extra[0].charCodeAt(0);
+  szyfrLiczba > 96 && szyfrLiczba < 123
+    ? (szyfrLiczba -= 97) && (extraLiczba -= 97)
+    : (szyfrLiczba -= 65) && (extraLiczba -= 65);
+  const nowyKlucz = (szyfrLiczba - extraLiczba + 26) % 26;
+  return nowyKlucz;
+}
+
+function cezarKryptoanaliza(tekst) {
+  const decrypted = [];
+  for (let i = 1; i < 26; i++) {
+    const zaszyfrowane = cezarOdszyfrowanie(tekst, i);
+    decrypted.push(zaszyfrowane);
+  }
+  return decrypted.join("\n");
 }
 
 function afinicznySzyfrowanie(tekst, a, przesuniecie) {
@@ -201,6 +255,68 @@ function afinicznyOdszyfrowanie(tekst, a, przesuniecie) {
   return zaszyfrowane;
 }
 
+function afinicznyKryptoanalizaZTekstem(tekst, extra) {
+  if (extra.length < 2) return "Za mało liter w tekście jawnym";
+  const mozliweKlucze = [];
+
+  for (let i = 0; i < extra.length; i++) {
+    for (let b = 0; b < 26; b++) {
+      for (let a = 1; a <= 26; a++) {
+        if (modInverse(a, 26) !== -1) {
+          if (extra[i].charCodeAt(0) > 96 && extra[i].charCodeAt(0) < 123) {
+            if (
+              ((a * (extra[i].charCodeAt(0) - 97) + b) % 26) + 97 ===
+              tekst[i].charCodeAt(0)
+            ) {
+              mozliweKlucze.push([a, b]);
+            }
+          } else if (
+            extra[i].charCodeAt(0) > 64 &&
+            extra[i].charCodeAt(0) < 91
+          ) {
+            if (
+              ((a * (extra[i].charCodeAt(0) - 65) + b) % 26) + 65 ===
+              tekst[i].charCodeAt(0)
+            ) {
+              mozliweKlucze.push([a, b]);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  const unikalneKlucze = mozliweKlucze.reduce((acc, curr) => {
+    const elem = acc[curr.flat()] ?? 0;
+    return {
+      ...acc,
+      [curr]: elem + 1,
+    };
+  }, {});
+  const klucz = Object.keys(unikalneKlucze)
+    .reduce((a, b) => (unikalneKlucze[a] > unikalneKlucze[b] ? a : b))
+    .split(",")
+    .map((x) => parseInt(x));
+  return [klucz, afinicznyOdszyfrowanie(tekst, klucz[1], klucz[0])];
+}
+
+function afinicznyKryptoanaliza(tekst) {
+  const decrypted = [];
+  for (let a = 1; a <= 26; a++) {
+    if (modInverse(a, 26) === -1) {
+      continue;
+    }
+    for (let b = 0; b < 26; b++) {
+      if (
+        afinicznyOdszyfrowanie(tekst, a, b) !== "Nie ma odwrotności do liczby a"
+      ) {
+        decrypted.push(afinicznyOdszyfrowanie(tekst, a, b));
+      }
+    }
+  }
+  return decrypted.join("\n");
+}
+
 function modInverse(a, m) {
   a = ((a % m) + m) % m;
   for (let x = 1; x < m; x++) {
@@ -209,23 +325,4 @@ function modInverse(a, m) {
     }
   }
   return -1;
-}
-
-function cezarKryptoanalizaZTekstemJawnym(tekst, extra) {
-  let szyfrLiczba = tekst[0].charCodeAt(0);
-  let extraLiczba = extra[0].charCodeAt(0);
-  szyfrLiczba > 96 && szyfrLiczba < 123
-    ? (szyfrLiczba -= 97) && (extraLiczba -= 97)
-    : (szyfrLiczba -= 65) && (extraLiczba -= 65);
-  const nowyKlucz = (szyfrLiczba - extraLiczba + 26) % 26;
-  return nowyKlucz;
-}
-
-function cezarKryptoanaliza(tekst) {
-  const decrypted = [];
-  for (let i = 1; i < 26; i++) {
-    const zaszyfrowane = cezarOdszyfrowanie(tekst, i);
-    decrypted.push(zaszyfrowane);
-  }
-  return decrypted.join("\n");
 }
