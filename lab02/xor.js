ed1f09c97bc9427e443a428c6635e74b563c71af
olewna <dajcie.suba2002@gmail.com>
2023-03-24 12:37:30 +0100

    xor initial commit

--- lab02/xor.js
+++ lab02/xor.js
@@ -3,17 +3,24 @@
 
 const fs = require("fs");
 
+let option;
+
 switch (process.argv[2]) {
   case "-p":
+    option = "p";
     console.log("Opcja -p");
     break;
   case "-e":
+    option = "e";
     console.log("Opcja -e");
     break;
   case "-k":
+    option = "k";
     console.log("Opcja -k");
     break;
   default:
     console.log("Proszę wybrać odpowienią opcję.");
     process.exit(0);
 }
+
+const optionP = () => {};
